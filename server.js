f078b3f00d2e4b13b84673211a92d331cbf0f339
JJJJason <jason.tang880414@gmail.com>
2018-11-27 23:35:28 +0800

    update

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const compression = require('compression')
 const morgan = require('morgan')
 
 const app = express()
@@ -10,7 +9,6 @@ const PORT = 4444
 const staticFile = express.static('dist')
 
 app.use(morgan())
-app.use(compression)
 app.use(staticFile)
 app.use(history({
   disableDotRule:    true,
